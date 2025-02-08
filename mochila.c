de4da7837739643b9dc22f8a8cd9fbd0f8d46806
wendell yan <wendellyan1503@gmail.com>
2025-02-07 22:31:09 -0300

    código do problema da mochila 2D

---
+++ mochila.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int max(int a,int b){
+    if(a>b){
+        return a;
+    } else {
+        return b;
+    }
+}
+
+
+int maxMochila(int it,int W,int itens[][2]){
+    static int **matriz = NULL;
+
+    // Aloca memória apenas na primeira chamada,ou seja,quando ela for NULL
+    if (matriz == NULL) {
+        //construção das linhas da matriz com a quantidade de itens +1,pois a primeira linha da matriz vai ser manter 0,pois não existe item 0
+        matriz = (int **)malloc((it + 1) * sizeof(int *));
+        for (int i = 0; i <= it; i++) {
+            //o mesmo é feito com as colunas,pois a primeira coluna da matriz vai ser manter 0,já que não existe item com peso 0
+            matriz[i] = (int *)malloc((W + 1) * sizeof(int));
+        }
+    }
+
+    for(int i = 0; i <= it; i++){
+        for(int w = 0; w <= W; w++){
+            //"caso base",preenche a primeira linha e a primeira coluna da matriz com 0
+            if(i==0 || w==0){
+                matriz[i][w]=0;
+                //Acesso a matriz de itens na linha 0(i -1,já que aqui o i=1) na coluna de índice 1,ou seja,acesso o peso na matriz de itens
+                //Se o peso do item atual for maior que a capacidade(w), o item não é incluído
+            } else if(w<itens[i-1][1]){
+                //a matriz naquele índice recebe o valor da matriz na linha anterior na coluna de índice w
+                //EM PRIMEIRO CASO,RECEBERIA O 0
+                matriz[i][w] = matriz[i-1][w];
+                //ùltima verificação: quando a capacidade(w) é menor ou igual ao peso do item atual(buscado na matriz de itens na linha i-1 na coluna 1)
+            } else {
+//aqui é realizada a comparação de quem é maior,o valor da matriz na linha anterior na coluna de índice w
+//ou o valor da matriz na linha anterior na coluna de índice w menos o peso do item somado ao valor do item atual
+                //escolhe o valor máximo entre incluir ou não o item
+                matriz[i][w] = max(matriz[i - 1][w],matriz[i - 1][w - itens[i - 1][1]] + itens[i - 1][0]);
+            }
+
+
+        }
+    }        
+
+//retornará o máximo valor que pode ser colocado na mochila
+    return matriz[it][W];
+
+}
+
+
+
+
+int main(){
+
+    int W = 5;
+    int itens[][2] = {
+        {12,2},
+        {10,1},
+        {20,3},
+        {15,2}
+    };
+    int it = sizeof(itens) / sizeof(itens[0]);
+    int mochila = maxMochila(it,W,itens);
+    printf("valor maximo da mochila: %d\n",mochila);
+
+
+    return 0;
+}
--- wendellyan_202300084030_transportadora.c
+++ wendellyan_202300084030_transportadora.c
@@ -1,85 +1,75 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+    char placa[8];
+    int peso;
+    int capacidade;
+}Veiculo;
+
+typedef struct {
+    char codigo[14];
+    float preco;
+    int p;
+    int volume;
+}Pacote;
 
 
-// typedef struct {
-//     int item;
-//     int valor;
-//     int peso;
-// } Itens;    
 
 
 
 
-//problema da mochila
-/*
-uma mochila possui n itens,cada item(i) tem um peso(w) e um valor(v)
-o objetivo é maximizar o valor total da mochila sem exceder o peso maximo(W)
-onde i>=1 e i<=n e w>=1 e w<=W
 
-
-
-*/
-
-void somaItens(int it,int w,int W){
-    static int **matriz = NULL;
-    matriz = (int**)malloc((it+1)*sizeof(int*));
-    if(matriz == NULL){
-        printf("Erro");
+int main(int argc, char *argv[]) {
+    FILE* arquivo = fopen(argv[1], "r");
+    if (arquivo == NULL) {
+        printf("ERRO\n");
+        return 0;
     }
-    matriz[0] = (int *)malloc((it+1)*(W+1)* sizeof(int));
-    if(matriz[0] == NULL){
-        printf("Erro");
-    }
-    //matriz[i] precisa apontar para o início da linha i, que está i * colunas posições depois de matriz[0].
-    //Se não ajustarmos os ponteiros, matriz[i] conterá lixo e matriz[i][j] levará a acesso inválido ou segmentation fault
-    for (int i = 1; i < it+1; i++) {
-    matriz[i] = matriz[0] + i * (W+1);
+    FILE* saida = fopen(argv[2], "w");
+    if (saida == NULL) {
+        printf("ERRO\n");
+        return 0;
     }
 
-    if(it ==0 || w == 0){
-        for(int i = 0; i <= it; i++){
-            matriz[i][0] = 0;
-        }
-        for(int j = 0; j <= W; j++){
-            matriz[0][j] = 0;
-        }
-    }
-
+    int numeroVeiculos, numeroPacotes;
     
 
-
-    for(int i = 0;i<=it;i++){
-        for(int j =0;j<=W;j++){
-            printf("%3d ", matriz[i][j]);  
-
-        }
-        printf("\n");
+    fscanf(arquivo,"%d", &numeroVeiculos);
+    Veiculo* veiculo = (Veiculo*) malloc(numeroVeiculos * sizeof(Veiculo));
+    if (veiculo == NULL) {
+        printf("ERRO\n");
+        return 0;
+    }
+    for(int i = 0; i < numeroVeiculos; i++){
+        fscanf(arquivo,"%s %d %d",veiculo[i].placa,&veiculo[i].peso,&veiculo[i].capacidade);
     }
 
-}
+    fscanf(arquivo,"%d", &numeroPacotes);
+    Pacote* pacote = (Pacote*) malloc(numeroPacotes * sizeof(Pacote));
+    if (pacote == NULL) {
+        printf("ERRO\n");
+        return 0;
+    }
 
+    for(int j =0;j<numeroPacotes;j++){
+        fscanf(arquivo,"%s %f %d %d",pacote[j].codigo,&pacote[j].preco,&pacote[j].p,&pacote[j].volume);
+    }
+    
+    //saída no arquivo output
+    for(int i = 0; i < numeroVeiculos; i++){
+        fprintf(saida,"%s %d %d",veiculo[i].placa,veiculo[i].peso,veiculo[i].capacidade);
+        fprintf(saida,"\n");
+    }
+    for(int j =0;j<numeroPacotes;j++){
+        fprintf(saida,"%s %f %d %d",pacote[j].codigo,pacote[j].preco,pacote[j].p,pacote[j].volume);
+        fprintf(saida,"\n");
+    }
+    
 
-
-
-int main() {
-
-    /*
-    matriz 3 por 4
-    linha[0]=itens
-    linha[1]=valores
-    linha[2]=pesos
-*/
-    // int itens [3][4] = {
-    //     {1, 2, 3 , 4},
-    //     {12, 10, 20, 15},
-    //     {2, 1, 3 , 2}
-    // }
-
-    printf("MATRIZ INICIADA\n");
-    somaItens(4,0,5);
-
-
+    fclose(arquivo);
+    fclose(saida);
 
     return 0;
 }
